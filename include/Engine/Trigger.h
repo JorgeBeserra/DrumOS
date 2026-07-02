93db6c683b4bec824d24c9552ce79b36bd6a7688
Jorge Beserra <jorgebeserra@gmail.com>
2026-07-02 07:17:56 -0300

    Add trigger statistics API

--- include/Engine/Trigger.h
+++ include/Engine/Trigger.h
@@ -5,6 +5,17 @@
 namespace DrumOS {
 namespace Trigger {
 
+struct PadStats {
+  uint32_t hits;
+  uint32_t crosstalkBlocked;
+  uint32_t retriggerBlocked;
+  uint32_t noiseRejected;
+  uint32_t peakSum;
+  uint32_t velocitySum;
+  uint16_t minPeak;
+  uint16_t maxPeak;
+};
+
 void begin();
 void process();
 
@@ -17,6 +28,12 @@ void resetCrosstalkMatrix();
 void setCrosstalkLevel(int sourcePad, int targetPad, int level);
 int getCrosstalkLevel(int sourcePad, int targetPad);
 void printCrosstalkMatrix();
+
+const PadStats& getStats(int pad);
+void resetStats();
+void resetStats(int pad);
+void printStats();
+void printStats(int pad);
 
 }
 }
